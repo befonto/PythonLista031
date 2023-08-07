87d93ce2edcd7074359c336eff842e7a61d4a983
Beatriz Fontoura <befonto@gamil>
2023-08-07 11:39:36 -0300

    Interpolação de Strings e Operadores Aritméticos

---
+++ interpolacao_strings/interpolacao_string_float_fstring.py
@@ -0,0 +1,38 @@
+valor1 = 8.75217
+valor2 = 73932.6
+valor3 = 11.349
+
+# formatação apenas como valor float
+print(f"Valor 1: {valor1:f}")
+
+# formatação float com decimal em 2 dígitos
+print(f"Valor 1: {valor1:.2f}") # :.2: 2 digitos dps da virgula
+print(f"Valor 2: {valor2:.2f}")
+print(f"Valor 3: {valor3:.2f}")
+
+# formatação float com separador de milhares e decimal em 2 dígitos
+print(f"Valor 2: {valor2:,.2f}")
+
+# formatação float, com total de 10 dígitos (números e ponto), sendo 2 decimais.
+print(f"Valor 1: {valor1:10.2f}")
+print(f"Valor 2: {valor2:10.2f}")
+print(f"Valor 3: {valor3:10.2f}")
+
+# semelhante ao anterior, mas preenche casas antes do ponto com zero quando necessário
+print(f"Valor 1: {valor1:10.2f}")
+print(f"Valor 2: {valor2:10.2f}")
+print(f"Valor 3: {valor3:10.2f}")
+
+valor04 = 0.7536
+
+# Formatação float exibindo em percentual com decimal em 1 dígito
+print(f"Valor 4: {valor04:.1%}")
+
+# Incluindo R$ antes do valor e substituindo a vírgula do milhar por underline
+texto_valor2 = f"R$ {valor2:_.2f}"
+print(f"Texto Valor 2: {texto_valor2}")
+
+# Substituindo o que é ponto por vírgula e o que é underline por ponto
+texto_valor_br = texto_valor2.replace(".",",").replace("_",".")
+print(f"Texto Valor BR: {texto_valor_br}")
+
---
+++ interpolacao_strings/interpolacao_string_int_fstring.py
@@ -0,0 +1,11 @@
+num1 = 6
+num2 = 382
+
+# simboliza-se a formação int com :d (de dígito)
+print(f"num1: {num1:d}")
+
+print(f"num1: {num1:7d}")
+print(f"num2: {num2:7d}")
+
+print(f"num1: {num1:07d}") # int com 7 dígitos, preenchendo com zeros.
+print(f"num2: {num2:07d}")
